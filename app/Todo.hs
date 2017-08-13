5c94cedf93ec472de22ed1728a1db283b9dc9358
Fumiaki Kinoshita <fumiexcel@gmail.com>
2017-08-13 21:36:06 +0900

    app/Todo: simplify

--- app/Todo.hs
+++ app/Todo.hs
@@ -10,13 +10,13 @@ main :: IO ()
 main = start $ Auto
   { autoWatch = Down KeyEnter
   , autoView = VRec
-    $ #box @:> Sized 0.1 (Fill (V4 120 0.3 0.3 1)
-      , TextBox)
+    $ #box @:> Sized 0.1 (fillRGBA 0.1 0.2 0.15 1, TextBox)
     <: #list @:> Unsized (Self $ Rows
       $ map Str ["Turn my swag on"])
     <: nil
-  , autoUpdate = \_ s -> let str = s ^. #box . _2 . textBoxText
-      in s
-        & #list . self %~ (\(Rows xs) -> Rows $ Str str : xs)
-        & #box . _2 .~ Right ("", 0)
+  , autoUpdate = \_ s -> case s ^. #box . _2 . textBoxText of
+    "" -> s
+    str -> s
+      & #list . self %~ (\(Rows xs) -> Rows $ Str str : xs)
+      & #box . _2 . textBoxText .~ ""
   }
